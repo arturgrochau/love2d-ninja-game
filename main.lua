5d5e68383e8747ed34e4e16056c824dfba512841
LucasXD <lucas.pereira@ort.org.br>
2020-07-02 07:37:19 -0300

    0.01
    3 funções iniciais para testar o desenvolvimento com o github.

---
+++ main.lua
@@ -0,0 +1,1 @@
+dofile("ninjacut.lua")
---
+++ ninjacut.lua
@@ -0,0 +1,33 @@
+--[[ O jogo vai se tratar de um ninja controlado pelo player no canto da tela que mata outros ninjas que correm para cima dele vindo do canto direito.
+     O usuário passará de fase quando atingir certo número de pontos que, por sua vez, são auferidos ao abater outros ninjas.
+     
+    Vai começar com uma imagem com um som tocando. Qualquer pressionar de tecla ou botão destravará a tela.
+    A opção de níveis aparece.
+      Cada nível só será desbloqueado a partir da finalização do anterior.
+      
+    Quando um nível carregar outro som tocará, a imagem de fundo mudará e os personagens serão introduzidos.
+
+]]
+
+--[[ O que está sendo feito: Tela de menu e inserção de som inicial.
+
+]]
+
+function love.load()
+
+
+
+end
+
+
+function love.update()
+  
+  
+  
+end
+
+
+function love.draw()
+  
+end  
+  
