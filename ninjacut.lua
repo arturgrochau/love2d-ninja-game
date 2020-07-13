1fea45ea11e49eb17cd9b1dd6350ea7ea97b0029
Slender <lucaz60@yahoo.com.br>
2020-07-13 07:10:27 -0300

    Revert "Update ninjacut.lua"
    This reverts commit 01aa930d651b4d31b57bc0f4cb467bdc784dd77a, reversing
    changes made to fbda33c813d15e10a5afd8d01f228fbd638e0082.

--- ninjacut.lua
+++ ninjacut.lua
@@ -121,7 +121,6 @@ local acertos = 0
 local vidas = 3
 
 
-
 function love.load()
   font = love.graphics.setNewFont("CHINESETAKEAWAY.ttf",48)  -- font título menu
   font2 = love.graphics.setNewFont("CHINESETAKEAWAY.ttf",27) -- font 
@@ -251,10 +250,6 @@ function love.draw()
     --love.graphics.draw(cut_ninja, 50, 580)
     love.graphics.rectangle( "line",  ninja2_x, ninja2_y, 200,160 ) 
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> cab32cac2204b61103910c90a7404acea410082c
     
     
    
@@ -300,9 +295,9 @@ function love.draw()
       
       
     -- se o usuário apertar a barra de espaço nesse meio tempo ele vai para próxima tela (estado +=1) 
-      if pressed then 
+      click_cut = love.mouse.isDown(1)
+      if love.keyboard.isDown("space") or click_cut then 
         love.audio.play(cut_song)
-<<<<<<< HEAD
         pontos = pontos +  1
         acertos = acertos +1
         ninja2_x = 850
@@ -317,17 +312,6 @@ function love.draw()
       
       
       
-=======
-        pontos = pontos + 1
-        if (pontos < 20) then
-          ninja2_x = 1200
-          estado = 1
-        else
-          estado = 3
-        end
-      end
-      
->>>>>>> cab32cac2204b61103910c90a7404acea410082c
     elseif ninja2_x < 50 or (ninja2_x > 300 and ninja2_x < 1100 and pressed) then
       love.audio.play(cut_song)
       estado = 2 -- vai para o estado de derrota      
